8c1b4d5e94f7217b569b5d78c4bc43d360aef547
akshay184 <akshayagarwal184@gmail.com>
2021-01-15 13:31:00 +0530

    pug done

--- Working-with-express/routes/admin.js
+++ Working-with-express/routes/admin.js
@@ -9,10 +9,7 @@ const router = express.Router();
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  // console.log("In the middleware");
-  // res.send('<form action = "/admin/add-product" method = "POST"><input type = "text"
-  // name ="title"><button type = "submit">Add Product</button></input></form>');
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render('add-product', {path: '/admin/add-product', pageTitle: 'Add Product'});
 });
 
 router.post("/add-product", (req, res, next) => {
